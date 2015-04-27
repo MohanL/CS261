01287238e9d0f08674b5b32fe11e84897631c09f
ssun10 <ssun10@u.rochester.edu>
2015-04-27 00:20:41 -0400

    Update Show.php

--- ssun10/Show.php
+++ ssun10/Show.php
@@ -16,6 +16,12 @@ $value = $_GET['value'];
 
    <body>
 	<h2>Show</h2>
+<!-- Insertion code -->
+<FORM NAME = "form2" METHOD =" " ACTION = "">
+<INPUT TYPE = "Submit" Name = "Update" VALUE = "Update"
+onclick="window.location.href='http://betaweb.csug.rochester.edu/~mliu26/CS261/ssun10/update.php'" />
+</FORM>
+<!-- Insertion code -->
 <?php
 // connect to the database 
 $dbtype = 'mysql';
