78ec7c4d50914c204022b941747c7db02352c05f
ssun10 <ssun10@u.rochester.edu>
2015-04-27 01:44:37 -0400

    Update list.php

--- ssun10/list.php
+++ ssun10/list.php
@@ -35,13 +35,6 @@ Diagnosis ID:<br>
 <INPUT TYPE = "Submit" Name = "Submit1" VALUE = "search">
 </FORM>
 
-<FORM METHOD ="link" ACTION = "http://betaweb.csug.rochester.edu/~mliu26/CS261/ssun10/insertion.php">
-<INPUT TYPE = "Submit" Name = "Insert" VALUE = "Insert">
-</FORM>
-
-<FORM METHOD ="link" ACTION = "http://betaweb.csug.rochester.edu/~mliu26/CS261/ssun10/deletion.php">
-<INPUT TYPE = "Submit" Name = "Delete" VALUE = "Delete">
-</FORM>
 <?php
 // connect to the database 
 $dbtype = 'mysql';
@@ -119,5 +112,16 @@ echo "</tr>\n";
 }              
 echo '</table>';
 ?>
+
+<FORM METHOD ="link" ACTION = "http://betaweb.csug.rochester.edu/~mliu26/CS261/ssun10/insertion.php">
+Click here to the Insertion Page
+<INPUT TYPE = "Submit" Name = "Insert" VALUE = "Insert">
+</FORM>
+
+<FORM METHOD ="link" ACTION = "http://betaweb.csug.rochester.edu/~mliu26/CS261/ssun10/deletion.php">
+Click here to the Deletion Page
+<INPUT TYPE = "Submit" Name = "Delete" VALUE = "Delete">
+</FORM>
+
     </body>
 </html>   
