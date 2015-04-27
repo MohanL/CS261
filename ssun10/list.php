38f2c0371b1e4af10155453abaa9dfbc0a4d2074
ssun10 <ssun10@u.rochester.edu>
2015-04-27 00:49:01 -0400

    Update list.php

--- ssun10/list.php
+++ ssun10/list.php
@@ -112,7 +112,7 @@ echo "<td class='firstrow'>$Diag_Date</td>";
 echo "<td class='firstrow'>$Med_id</td>";
 echo "<td class='firstrow'>$Remark</td>";
 echo "<td class='firstrow'>$Second_Diag_Date</td>";
-echo "<td class='firstrow'><a href='list_supplementary.php?value=$Diag_Id'>$link</a>
+echo "<td class='firstrow'><a href='show.php?value=$Diag_Id'>$link</a>
 </div>
 </td>";
 echo "</tr>\n"; 
